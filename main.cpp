e7cd021943f44cf9995d3646c75bed601b7d9996
quincynash <quincy.nash@icloud.com>
2025-11-20 00:44:59 -0800

    Quick test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-int main() { return 0; }
+#include "renderer.hpp"
+
+const std::string OUTPUT_FILE = "output.ppm";
+
+int main() {
+  Scene scene(512, 512, 6);
+  scene.setBackground(135, 206, 235);  // Sky blue
+  // Need to specify properties in the future
+
+  scene.render(OUTPUT_FILE);
+  return 0;
+}
