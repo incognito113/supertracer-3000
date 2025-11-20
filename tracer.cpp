1108fa25006084e6610d03bfa7e482ef742f2200
quincynash <quincy.nash@icloud.com>
2025-11-20 00:44:02 -0800

    Add ray calculation relative to camera

--- tracer.cpp
+++ tracer.cpp
@@ -1,3 +1,30 @@
 #include "tracer.hpp"
 
 Vector Ray::at(double t) const { return orig + dir * t; }
+
+Ray Camera::getRay(const int i, const int j, const int width,
+                   const int height) const {
+  // Normalize corrdinates to center of pixel
+  double u = (i + 0.5) / width;
+  double v = (j + 0.5) / height;
+
+  // Compute size of "screen" based on fov
+  double halfHeight = tan(fov * 0.5 * M_PI / 180.0);
+  double halfWidth = (width / (double)height) * halfHeight;
+
+  // Camera basis vectors
+  Vector right = direction.cross(Vector(0, 1, 0)).norm();
+  Vector up = right.cross(direction).norm();
+  Vector forward = direction.norm();
+
+  // Image plane coordinates relative to camera
+  double x = (2 * u - 1) * halfWidth;
+  double y = (1 - 2 * v) * halfHeight;
+  double z = 1;  // distance of screen from camera
+
+  // Map to world coordinates
+  Vector worldPoint = position + x * right + y * up + z * forward;
+  Vector dir = (worldPoint - position).norm();
+
+  return Ray(position, dir);
+}
--- tracer.hpp
+++ tracer.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "color.hpp"
 #include "vector.hpp"
+
+struct Light {
+  Vector position;
+  Color color;
+};
 
 class Ray {
  public:
@@ -12,4 +18,13 @@ class Ray {
 
   // Returns the point along the ray at distance t from the origin
   Vector at(double t) const;
+};
+
+class Camera {
+ public:
+  Vector position;
+  Vector direction;  // Not necessarily normalized
+  double fov;        // In degrees
+
+  Ray getRay(const int x, const int y, const int width, const int height) const;
 };
