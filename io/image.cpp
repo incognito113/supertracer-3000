90f8fb6820d9b70b4a44620c980eda96cc49aab2
quincynash <quincy.nash@icloud.com>
2025-11-29 23:32:00 -0800

    Add file dialog and use raw ppm

--- io/image.cpp
+++ io/image.cpp
@@ -2,21 +2,47 @@
 
 #include <fstream>
 
+#include "io/tinyfiledialogs.hpp"
 #include "renderer/tracer.hpp"
 
-// Render the scene to a ppm file
-void Image::render(const std::string& filename) const {
-  std::ofstream output(filename);
+Image::Image(const Scene& sc) : scene(sc), pixels(*(new std::vector<Color>())) {
+  Tracer tracer(scene);
+  pixels = tracer.render_pixels(scene);
+}
+
+// Save the image to a PPM file (return true on success)
+// If filename is empty, prompts user for filename
+bool Image::save(std::string filename) const {
+  char* saveFile;
+
+  if (filename.empty()) {
+    // Get filename from user from tinyfiledialogs
+    saveFile = tinyfd_saveFileDialog("Save file",     // title
+                                     "./output.ppm",  // default name
+                                     0, NULL,         // no filters
+                                     "Text Files"     // description
+    );
+    if (!saveFile) return false;
+  } else {
+    saveFile = const_cast<char*>(filename.c_str());
+  }
+
+  std::ofstream output(saveFile, std::ios::binary);
   if (!output.is_open()) {
-    throw std::runtime_error("Failed to open output file: " + filename);
+    return false;
   }
 
   // PPM format:
-  output << "P3" << std::endl;  // ASCII format
-  output << scene.getWidth() << " " << scene.getHeight() << std::endl; // dimensions of the image
-  output << "255" << std::endl; // max RGB value for a given pixel
-  for (const Color& color : pixels) {
-    output << color.get255String() << std::end1; // store each pixel in a line of rgb values
+  // P6
+  // <width> <height>
+  // 255
+  // r g b r g b r g b ...
+  output << "P6" << std::endl;  // Binary PPM
+  output << scene.getWidth() << " " << scene.getHeight() << std::endl;
+  output << "255" << std::endl;
+  for (Color px : pixels) {
+    output.write(reinterpret_cast<const char*>(px.getBytes().data()), 3);
   }
   output.close();
+  return true;
 }
