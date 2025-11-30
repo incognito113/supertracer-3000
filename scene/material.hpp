b269746896b657ad8e017bb34f25631c9fd58222
quincynash <quincy.nash@icloud.com>
2025-11-29 23:37:58 -0800

    Better naming convention

--- scene/material.hpp
+++ scene/material.hpp
@@ -4,8 +4,9 @@
 
 // Stores matieral properties for shapes
 struct Material {
-  const Color diffuse;
-  const Color specular;
-  const double shininess;
+  const Color color;
+  const Color specular = Color(255, 255, 255);
+  const double specularFactor = 0.5;
+  const double shininess = 8.0;
   const double reflectivity;
 };
