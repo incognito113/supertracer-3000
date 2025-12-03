989ef6cefe6ea5196a9179ee3695353cb184967f
= <asatheamazing@gmail.com>
2025-12-03 14:04:22 -0800

    added an obj file importer

---
+++ io/mesh.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+#include "scene/material.hpp"
+#include "scene/scene.hpp"
+
+bool importMesh(const Scene scene, const std::string fileName, const Material material);
+// takes an input .obj file and loads all of its tris with user-defined material into a scene
