8268d81547baf657fe4d17b9eb38cb8b7b444af1
quincynash <quincy.nash@icloud.com>
2025-12-03 15:20:10 -0800

    Use const, add Bounds

--- shapes/shape.hpp
+++ shapes/shape.hpp
@@ -7,44 +7,81 @@
 #include "math/vector.hpp"
 #include "scene/material.hpp"
 
+// Stores information about collisions between rays and shapes
 struct HitInfo {
-    Vector pos;
-    Vector normal;
-    Ray ray;
-    double t;
-    const Material* material;
+  const Vector pos;     // Point of intersection
+  const Vector normal;  // Surface normal at intersection
+  const Ray ray;        // Ray that caused the hit
+  const double t;
+  const Material* material;
 
-    HitInfo(const Vector& p,
-            const Vector& n,
-            const Ray& r,
-            double tval,
-            const Material* m)
-        : pos(p),
-          normal(n.norm()),   // ensure normalized
-          ray(r),
-          t(tval),
-          material(m) {}
-
-    HitInfo(const HitInfo& other)
-        : pos(other.pos),
-          normal(other.normal),
-          ray(other.ray),
-          t(other.t),
-          material(other.material) {}
+  HitInfo(const Vector& p, const Vector& n, const Ray& r, double tval,
+          const Material* m)
+      : pos(p), normal(n), ray(r), t(tval), material(m) {};
+  HitInfo(const HitInfo& other)
+      : pos(other.pos),
+        normal(other.normal),
+        ray(other.ray),
+        t(other.t),
+        material(other.material) {};
 };
 
+// Forward declaration
+class Tracer;
+
+// Abstract base class for all shapes in the scene
 class Shape {
-public:
-    static constexpr double EPS = 1e-6;
+ protected:
+  const Material material;
 
-public:
-    Material material;
+ public:
+  Shape(const Material& mat) : material(mat) {}
 
-    Shape(const Material& mat) : material(mat) {}
+  // Returns HitInfo if intersection, std::nullopt otherwise
+  virtual std::optional<HitInfo> intersects(const Ray& ray) const = 0;
+  virtual Shape* clone() const = 0;
 
-    virtual std::optional<HitInfo> intersects(const Ray& ray) const = 0;
+  virtual ~Shape() = default;
 
-    virtual Shape* clone() const = 0;
+  friend class Tracer;
+};
 
-    virtual ~Shape() {}
+struct Bounds {
+ private:
+  void compCenter();
+  void compArea();
+
+ public:
+  Vector min;
+  Vector max;
+  Vector center;
+  double area;
+
+  Bounds()
+      : min(std::numeric_limits<double>::max()),
+        max(-std::numeric_limits<double>::max()),
+        center(),
+        area(0) {}
+  Bounds(const Vector& point)
+      : min(point), max(point), center(point), area(0) {}
+  Bounds(const Vector& bmin, const Vector& bmax) : min(bmin), max(bmax) {
+    compCenter();
+    compArea();
+  }
+
+  void expand(const Bounds& other);
+  void expand(const Vector& point);
+  bool intersects(const Ray& ray, double& tmin, double& tmax) const;
+
+  ~Bounds() = default;
+};
+
+class BoundedShape : public Shape {
+ public:
+  Bounds bounds;
+
+  BoundedShape(const Material& mat, const Vector& bmin, const Vector& bmax)
+      : Shape(mat), bounds(bmin, bmax) {}
+
+  virtual ~BoundedShape() = default;
 };
