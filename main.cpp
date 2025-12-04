96e948a8785d43b2cc13473c8e840bf11371ddd9
quincynash <quincy.nash@icloud.com>
2025-12-04 12:05:57 -0800

    bunny and camera test

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ int main() {
   Scene scene{512, 512, 6};
 
   scene.setBackground(135, 206, 235);
-  scene.setCamera(Vector(0.5, -1.0, 0.5), Vector(0, 1, 0), 60.0);
+  scene.setCamera(Vector(0, 0.5, 1.5), Vector(0, 0, -1), 60.0);
   scene.setAmbientLight(0.2);
-  scene.addLight(Vector(0, -0.5, 1.0), Color(255, 255, 255));
+  scene.addLight(Vector(0, 0, 10.0), Color(255, 255, 255));
 
   scene.importOBJ(Vector(0.0, 0.0, 0.0), "bunny.obj", 5,
-                  Material{.color = Color(255, 0, 0), .reflectivity = 0.2});
+                  Material{
+                      .color = Color(255, 0, 0),
+                      .reflectivity = 0.0,
+                  });
 
   std::cout << "Scene has " << scene.shapeCount() << " shapes." << std::endl;
 
