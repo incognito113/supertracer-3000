165cc1f38d307d17b0d056c8e196f5443d0bb535
quincynash <quincy.nash@icloud.com>
2025-12-04 00:09:26 -0800

    OBJ test

--- main.cpp
+++ main.cpp
@@ -12,26 +12,10 @@ int main() {
   scene.setAmbientLight(0.2);
   scene.addLight(Vector(0, -0.5, 1.0), Color(255, 255, 255));
 
-  scene.addPlane(Vector(0, 0, 0), Vector(0, 0, 1),
-                 (Material){
-                     .color = Color(255, 255, 255),
-                     .reflectivity = 0.0,
-                 });
-  scene.addSphere(Vector(0.25, 0.45, 0.4), 0.4,
-                  (Material){
-                      .color = Color(255, 0, 0),
-                      .reflectivity = 0.3,
-                  });
-  scene.addSphere(Vector(1.0, 1.0, 0.25), 0.25,
-                  (Material){
-                      .color = Color(0, 255, 0),
-                      .reflectivity = 0.0,
-                  });
-  scene.addSphere(Vector(0.8, 0.3, 0.15), 0.15,
-                  (Material){
-                      .color = Color(0, 0, 255),
-                      .reflectivity = 0.7,
-                  });
+  scene.importOBJ(Vector(0.0, 0.0, 0.0), "bunny.obj", 5,
+                  Material{.color = Color(255, 0, 0), .reflectivity = 0.2});
+
+  std::cout << "Scene has " << scene.shapeCount() << " shapes." << std::endl;
 
   Renderer renderer{scene, 60};
   renderer.run();
