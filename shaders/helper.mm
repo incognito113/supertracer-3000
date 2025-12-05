0ed28ea571138bcdea7083f7f34d57fa4ff6667b
quincynash <quincy.nash@icloud.com>
2025-12-05 12:46:51 -0800

    Fix duplicate definition

--- shaders/helper.mm
+++ shaders/helper.mm
@@ -1,10 +1,6 @@
 #include "metal.hpp"
 #include <Metal/Metal.hpp>
 
-struct MetalCommandBuffer {
-  MTL::CommandBuffer *raw;
-};
-
 void MetalCompute::addHandler(MetalCommandBuffer *cb,
                               std::function<void()> fn) {
   cb->raw->addCompletedHandler(^(MTL::CommandBuffer *) {
