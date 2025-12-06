aed48170cf811ef44294a02494b1f91e27ab51f2
quincynash <quincy.nash@icloud.com>
2025-12-05 21:40:41 -0800

    Preallocate readonly buffers for performance

--- shaders/metal.hpp
+++ shaders/metal.hpp
@@ -1,31 +1,40 @@
-#ifdef METAL
-
 #pragma once
+
+#ifdef METAL
 
 #include <Metal/Metal.hpp>
 #include <functional>
 #include <string>
 #include <vector>
 
+#include "shaders/conversions.hpp"
+
 class MetalCompute {
  private:
   MTL::Device* device;
   MTL::Library* lib;
   MTL::CommandQueue* queue;
+  MTL::ComputePipelineState* pipeline;
+
+  MTL::Buffer* sceneDataBuf = nullptr;
+  MTL::Buffer* materialsBuf = nullptr;
+  MTL::Buffer* lightsBuf = nullptr;
+  MTL::Buffer* nodesBuf = nullptr;
+  MTL::Buffer* trianglesBuf = nullptr;
+  MTL::Buffer* spheresBuf = nullptr;
+  MTL::Buffer* planesBuf = nullptr;
+  MTL::Buffer* outputColorsBuf = nullptr;
 
   void addHandler(MTL::CommandBuffer* cb, std::function<void()> fn);
 
  public:
   MetalCompute();
 
-  template <typename... Ts>
-  void runKernel(const std::string& kernelName, std::function<void()> callback,
-                 std::vector<Ts>&... vectors);
-  template <typename... Ts>
-  void runKernel(const std::string& kernelName, std::vector<Ts>&... vectors);
+  void init(Converter::GPU_Data& gpuData);
+  void raytrace(Converter::GPU_SceneData& sceneData,
+                std::function<void(simd_float3*, size_t)> callback);
 
   ~MetalCompute();
 };
 
-#include "metal_impl.hpp"
 #endif
